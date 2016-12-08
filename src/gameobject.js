f7cce0591eb0c2224e4b74979c818aa1530ffa83
LMCMLJ <mjcalvey2@gmail.com>
2016-12-08 21:41:31 +0000

    Removes 'entity', adds 'gameobject'

---
+++ src/gameobject.js
@@ -0,0 +1,7 @@
+function GameObject() {
+  this.transform = {
+    position: [0, 0],
+    rotation: 0,
+    scale: [1, 1]
+  };
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,100 @@
-function World(screen) {
-  this.screen = screen;
-  this.tick = 0;
-  this.entity = new Entity();
+window.onload = function () {
+  var screen = document.getElementById("screen").getContext("2d");
+  var tick = 0;
+  newEntity = new Entity();
+  var entities = [newEntity];
+  var resolution = 32;
+  var canvasSize = 640;
 
-  var self = this;
   function gameLoop() {
-    self.update();
-    self.draw();
-    self.tick += 1;
+    tick += 1;
+    entities.forEach(function(entity) {
+      updatePosition(entity);
+    });
+    entities.forEach(function(entity) {
+      draw(entity);
+    });
     requestAnimationFrame(gameLoop);
   }
   gameLoop();
-}
+  window.addEventListener('keydown',doKeyDown,true);
 
-World.prototype.draw = function() {
-  screen.clearRect(0, 0, 1080, 1920);
-  screen.fillRect(this.x, this.y, 100, 100);
+  function updatePosition(entity) {
+    var head = entity.getHeadPosition();
+    entity.move(head);
+    entity.position.pop();
+
+    entity.checkBoundary();
+  }
+
+  function draw(entity) {
+    screen.clearRect(0, 0, canvasSize, canvasSize);
+    entity.position.forEach(function(cube) {
+      screen.fillRect(cube[0] * resolution, cube[1] * resolution, resolution, resolution);
+    });
+  }
+
+  function Entity() {
+    this.direction = 'down';
+    this.position = [[10, 10]];
+  }
+
+  Entity.prototype.getHeadPosition = function () {
+    return this.position[0];
+  };
+
+  Entity.prototype.checkBoundary = function () {
+    if (this.position[0][0] >= resolution){
+      this.position[0][0] = 0;
+    }
+    else if (this.position[0][1] >= resolution){
+      this.position[0][1] = 0;
+    }
+    else if (this.position[0][0] < 0){
+      this.position[0][0] = resolution - 1;
+    }
+    else if (this.position[0][1] < 0){
+      this.position[0][1] = resolution - 1;
+    }
+  };
+
+  Entity.prototype.move = function(head) {
+    if (this.direction === 'down'){
+      this.position.unshift([head[0], head[1] + 1]);
+    }
+    else if (this.direction === 'up'){
+      this.position.unshift([head[0], head[1] - 1]);
+    }
+    else if (this.direction === 'left'){
+      this.position.unshift([head[0] - 1, head[1]]);
+    }
+    else if (this.direction === 'right'){
+      this.position.unshift([head[0] + 1, head[1]]);
+    }
+  };
+
+  function doKeyDown(evt){
+    switch (evt.keyCode) {
+      case 38:
+        if(entity.direction != 'down'){
+          entity.direction = 'up';
+        }
+      break;
+      case 40:
+        if(entity.direction != 'up'){
+          entity.direction = 'down';
+        }
+      break;
+      case 37:
+        if(entity.direction != 'right'){
+          entity.direction = 'left';
+        }
+      break;
+      case 39:
+        if(entity.direction != 'left'){
+          entity.direction = 'right';
+        }
+      break;
+    }
+  }
 };
-
-World.prototype.newEntity = function(name) {
-  this.name = new Entity();
-};
-
-
-World.prototype.update = function() {
-  this.x += 1;
-  this.y += 1;
-};
-
-
-var screen = document.getElementById("screen").getContext("2d");
-var world = new World(screen);
